788c9090578b5c1fe289dd3d47f0f1b883f7688c
heojieun <gjekdmlwl@naver.com>
2022-06-28 13:40:51 +0900

    레이아웃 수정

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 import { createStore } from 'redux';
 
-const plus   = document.getElementById('plus');
-const minus  = document.getElementById('minus');
-const number = document.querySelector('span');
+const form  = document.querySelector("form");
+const input = document.querySelector("input");
+const ul    = document.querySelector("ul");
 
-const PLUS  = "PLUS";
-const MINUS = "MINUS";
+const ADD_TODO    = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const reducer = (state = 0, action) => {
+const reducer = (state = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case PLUS:
-      return state + 1;
-    case MINUS:
-      return state - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
       return state;
   }
 };
+
 const store = createStore(reducer);
 
-const onChange = () => {
-  console.log("change");
-  number.innerText = store.getState();
+const onSubmit = event => {
+  event.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
-store.subscribe(onChange);
-
-const plusHandler = () => {
-  store.dispatch({type: PLUS});
-}
-const minusHandler = () => {
-  store.dispatch({type: MINUS});
-}
-
-plus.addEventListener("click", plusHandler);
-minus.addEventListener("click", minusHandler);
+form.addEventListener("submit", onSubmit);
