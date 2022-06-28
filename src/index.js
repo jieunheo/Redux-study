9328747a762e888fb51f8d5425fcba174eb14d52
heojieun <gjekdmlwl@naver.com>
2022-06-28 13:37:05 +0900

    redux - redux store 만들기 - redux reducer 만들기 - redux store와 reducer 연결하기 - reducer에 값 전달하기(dispatch) - store 구독(변화 감지)하기 (subscribe) - 변수를 이용하여 오타 방지하기 - if문과 swich문 모두 사용 가능

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
+import { createStore } from 'redux';
+
 const plus   = document.getElementById('plus');
 const minus  = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerText = count;
-const updateText = () => {
-  number.innerText = count;
-}
+const PLUS  = "PLUS";
+const MINUS = "MINUS";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case PLUS:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+const onChange = () => {
+  console.log("change");
+  number.innerText = store.getState();
+};
+store.subscribe(onChange);
 
 const plusHandler = () => {
-  count = count + 1;
-  updateText();
-};
+  store.dispatch({type: PLUS});
+}
 const minusHandler = () => {
-  count = count - 1;
-  updateText();
-};
+  store.dispatch({type: MINUS});
+}
 
 plus.addEventListener("click", plusHandler);
 minus.addEventListener("click", minusHandler);
