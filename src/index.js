19f0020da750852b1357c8eede7cc8fcb43e31f2
heojieun <gjekdmlwl@naver.com>
2022-06-28 15:31:25 +0900

    dispatch 함수와 add/delete 함수 분리

--- src/index.js
+++ src/index.js
@@ -1,37 +1,57 @@
-import { createElement } from 'react';
 import { createStore } from 'redux';
 
+// element
 const form  = document.querySelector("form");
 const input = document.querySelector("input");
 const ul    = document.querySelector("ul");
 
+// 변수
 const ADD_TODO    = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
-const reducer = (state = [], action) => {
+// dispatch에 넣을 값 만들기 위한 함수
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+    id: Date.now()
+  }
+}
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
 
+// store reducer
+const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
       return [{text: action.text, id: action.id}, ...state];
     case DELETE_TODO:
-      const newState = state.filter(item => item.id !== action.id);
-      return newState;
+      return state.filter(todo => todo.id !== action.id);
     default:
       return state;
   }
 };
 
+// store 만들기 및 reducer 연결
 const store = createStore(reducer);
+// store가 변하면 호출할 함수
+store.subscribe(() => console.log(store.getState()));
 
-const deleteToDo = (event) => {
+// dispatch를 위한 함수
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+}
+const dispatchDeleteToDo = (event) => {
   const id = parseInt(event.target.parentNode.id);
-  store.dispatch({ type: DELETE_TODO, id });
-}
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text, id: Date.now() });
+  store.dispatch(deleteToDo(id));
 }
 
+// ul에 state값을 이용하여 li 넣어주기
 const paintTodos = () => {
   const toDos = store.getState();
   ul.innerHTML = '';
@@ -40,7 +60,7 @@ const paintTodos = () => {
     const btn = document.createElement('button');
 
     btn.innerText = "del";
-    btn.addEventListener("click", deleteToDo);
+    btn.addEventListener("click", dispatchDeleteToDo);
 
     li.id = todo.id;
     li.innerHTML = todo.text;
@@ -51,10 +71,11 @@ const paintTodos = () => {
 }
 store.subscribe(paintTodos);
 
+// 전송 시 실행할 함수
 const onSubmit = event => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 form.addEventListener("submit", onSubmit);
