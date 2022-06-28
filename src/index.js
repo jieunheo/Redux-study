fdffc560d831780aaddb50e24ff91d1daf05b9a9
heojieun <gjekdmlwl@naver.com>
2022-06-28 14:23:04 +0900

    todo add 구현

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ const ADD_TODO    = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 const reducer = (state = [], action) => {
+
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [{text: action.text, id: action.id}, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -21,10 +22,26 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+const paintTodos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(todo => {
+    const li = document.createElement('li');
+    li.id = todo.id;
+    li.innerHTML = todo.text;
+    ul.appendChild(li);
+  });
+}
+store.subscribe(paintTodos);
+
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text, id: Date.now() });
+}
+
 const onSubmit = event => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  addToDo(toDo);
 };
 form.addEventListener("submit", onSubmit);
