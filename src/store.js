60b360728d999afeee32530660aa1581f8921f7e
heojieun <gjekdmlwl@naver.com>
2022-06-29 15:36:22 +0900

    createReducer를 이용하여 switch case 코드 줄이기

--- src/store.js
+++ src/store.js
@@ -1,19 +1,28 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload.text, id: action.payload.id }, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload.text, id: action.payload.id }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload.text, id: action.payload.id })
+  },
+  [deleteToDo]: (state, action) => {
+    return state.filter(toDo => toDo.id !== action.payload);
   }
-};
+});
 
 const store = createStore(reducer);
 
