e80c7d5f2d60ed7e59e246f3a5d1aef98d0e5e7e
heojieun <gjekdmlwl@naver.com>
2022-06-29 11:35:19 +0900

    react에서 redux store 만들고 index에 연결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+import store from "./store";
 import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider value={store}>
+    <App />
+  </Provider>
+);
---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'react-redux';
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = text => {
+  return {
+    type: ADD,
+    text: text,
+    id: Date.now()
+  };
+}
+export const deleteToDo = id => {
+  return {
+    type: DELETE,
+    id: id
+  };
+}
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: action.id }, ...state];
+    case DELETE:
+      return state.filter(toDo => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
