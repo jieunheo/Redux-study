7944e2b700ca5991e92e4a7723737d9dbb09d5c0
heojieun <gjekdmlwl@naver.com>
2022-06-29 11:20:55 +0900

    입력 받은 값 가져오기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,27 @@
-export default () => "Home";
+import { Fragment, useState } from "react";
+
+const App = () => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const enteredTextHandler = (event) => {
+    setEnteredText(event.target.value);
+  }
+  const addToDoHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredText);
+    setEnteredText('');
+  };
+
+  return (
+    <Fragment>
+      <h1>To Do</h1>
+      <form onSubmit={addToDoHandler}>
+        <input type='text' value={enteredText} onChange={enteredTextHandler} />
+        <button type='submit'>ADD</button>
+      </form>
+      <ul></ul>
+    </Fragment>
+  )
+};
+
+export default App;
